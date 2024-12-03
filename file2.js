4e463b5aac6d13df617dcfb8ca9f387562b63277
khushii-13 <khushichaudhary904@gmail.com>
2024-12-03 10:47:33 +0530

    datatypes in js

---
+++ file2.js
@@ -0,0 +1,27 @@
+"use strict" //treat all js code as newer version (this is automated if you do not write it will eork also)
+
+//alert (2+4) //we are using node js not browser
+
+// tc39.js // echma script documentation and mdn documentation
+
+
+let name  = "khushi"
+let age =21
+let isLogged = true
+let emp = null
+let und 
+
+console.log(typeof(isLogged))
+console.log(typeof(und))  // undefined is a type of undefined
+console.log(typeof(emp)) // null is an object (imp for interview)
+//data types
+// -- Premetive data type
+// number  = range ->2^53
+// bigint
+// string =  "" , ''
+// boolean = true or false
+// null (stand alone value)
+// undefined 
+// symbol = unique
+
+//object
